96dffaa39e8a8ccc229cdc5d79ec2201e5a4937c
Julian <nichikuma.julian@gmail.com>
2026-08-25 11:56:54 -0300

    Configure tests  settings  to run on Docker build

--- src/test/java/br/com/fiap/restaurante/restaurante/RestauranteApplicationTests.java
+++ src/test/java/br/com/fiap/restaurante/restaurante/RestauranteApplicationTests.java
@@ -2,8 +2,11 @@ package br.com.fiap.restaurante.restaurante;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.util.Assert;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class RestauranteApplicationTests {
 
 	@Test
